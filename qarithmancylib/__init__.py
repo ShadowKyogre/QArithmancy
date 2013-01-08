e322d5bbdd8c0ac90efd3e6d069bd201be366344
ShadowKyogre <shadowkyogre@aim.com>
2013-01-08 10:37:36 -0800

    Include license and set up qarithmancy to be packaged using distutils for contexts that aren't download and run.

---
+++ qarithmancylib/__init__.py
@@ -0,0 +1,11 @@
+from os import path
+APPNAME="QArithmancy"
+APPVERSION="0.1"
+AUTHOR="ShadowKyogre"
+DESCRIPTION="Comprehensive numerology application using PyQt."
+YEAR="2013"
+PAGE="http://shadowkyogre.github.com/qarithmancy/"
+EMAIL="shadowkyogre.public@gmail.com"
+### CONSTANTS BEGIN ###
+DATA_DIR = path.dirname(path.dirname(__file__))
+### CONSTANTS END ###
